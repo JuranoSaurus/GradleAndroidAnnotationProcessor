3174d25667974b77806a6637f29c5e338d69a489
Francis <tjdrms0121@gmail.com>
2015-10-07 15:25:41 +0900

    Finish Annotation Processor

---
+++ GradleAndroidAnnotationProcessor/app/src/main/java/com/jurano/annotation/ConfirmFullScreen.java
@@ -0,0 +1,10 @@
+package com.jurano.annotation;
+
+import com.jurano.annotation.api.FullScreen;
+
+/**
+ * Created by SungGeun on 2015-10-07.
+ */
+@FullScreen
+public class ConfirmFullScreen {
+}
--- GradleAndroidAnnotationProcessor/compiler/src/main/java/com/jurano/annotation/compiler/AnnotationProcessor.java
+++ GradleAndroidAnnotationProcessor/compiler/src/main/java/com/jurano/annotation/compiler/AnnotationProcessor.java
@@ -4,14 +4,27 @@ import com.jurano.annotation.api.FullScreen;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 
 public class AnnotationProcessor extends AbstractProcessor{
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Collections.singleton(FullScreen.class.getCanonicalName());
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
+
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
         for(Element annotatedElement : roundEnvironment.getElementsAnnotatedWith(FullScreen.class)){
@@ -20,7 +33,7 @@ public class AnnotationProcessor extends AbstractProcessor{
             String annotatedClassName = annotatedClass.getSimpleName().toString();
 
             try {
-                FileOutputStream output = new FileOutputStream("D:\\log.txt");
+                FileOutputStream output = new FileOutputStream("E://log.txt");
                 output.write(annotatedClassName.getBytes());
                 output.close();
             } catch (IOException e) {
